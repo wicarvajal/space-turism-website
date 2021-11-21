3a067fe9d07e62276f940e7dbe6afaed1a136502
William Carvajal <wi.rcarvajal@gmail.com>
2021-11-21 00:38:17 -0300

    Navbar functionality & design complete. Barlow condensed font-face added. Home module & component added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { DestinationPageComponent } from './modules/destination/destination-page/destination-page.component';
+import { CrewPageComponent } from './modules/crew/crew-page/crew-page.component';
+import { TechnologyPageComponent } from './modules/technology/technology-page/technology-page.component';
+import { HomeComponent } from './modules/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'destination', component: DestinationPageComponent },
+  { path: 'crew', component: CrewPageComponent },
+  { path: 'technology', component: TechnologyPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
