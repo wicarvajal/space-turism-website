6198ef3b535dab7f3768b5e3368023036f62a669
William Carvajal <wi.rcarvajal@gmail.com>
2021-11-26 20:06:19 -0300

    Set backgroung image acoording window size.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Event, NavigationEnd, Router } from '@angular/router';
 import {filter} from 'rxjs/operators';
 import { EAvailableRoutes } from './enum/available-routes';
@@ -9,14 +9,28 @@ import { EAvailableRoutes } from './enum/available-routes';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'space-turism-website';
-  backgroundImage: string;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = event.target.innerWidth;
+    this.windowClasification = this.getWindowSizeClasification();
+    this.setBackgroundImage(this.currentUrl);
+  }
+
+  public title = 'space-turism-website';
+  public currentUrl: string;
+  public backgroundImage: string;
+  public windowClasification: string;
+  private innerWidth: number;
 
   constructor(private router: Router) {
     this.backgroundImage = '';
+    this.windowClasification = '';
+    this.currentUrl = '';
+    this.innerWidth = window.innerWidth;
   }
 
   ngOnInit(): void {
+    this.windowClasification = this.getWindowSizeClasification();
     this.listenRouterChanges();
   }
 
@@ -24,29 +38,36 @@ export class AppComponent implements OnInit {
     this.router.events
     .pipe(filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd))
     .subscribe((url: NavigationEnd)  => {
-      this.setBackgroundImage(url.urlAfterRedirects);
+      const cleanUrl = url.urlAfterRedirects.replace('/', '');
+      this.currentUrl = cleanUrl;
+      this.setBackgroundImage(cleanUrl);
     });
   }
 
   public setBackgroundImage(url: string) {
-    const cleanUrl = url.replace('/', '')
-    console.log(cleanUrl);
-    switch (cleanUrl) {
+    switch (url) {
       case EAvailableRoutes.HOME:
-        this.backgroundImage = '../assets/img/home/background-home-desktop.jpg';
+        this.backgroundImage = `../assets/img/home/background-home-${this.windowClasification}.jpg`;
         break;
       case EAvailableRoutes.DESTINATION:
-        this.backgroundImage = '../assets/img/destination/background-destination-desktop.jpg';
+        this.backgroundImage = `../assets/img/destination/background-destination-${this.windowClasification}.jpg`;
         break;
       case EAvailableRoutes.CREW:
-        this.backgroundImage = '../assets/img/crew/background-crew-desktop.jpg';
+        this.backgroundImage = `../assets/img/crew/background-crew-${this.windowClasification}.jpg`;
         break;
       case EAvailableRoutes.TECHNOLOGY:
-        this.backgroundImage = '../assets/img/technology/background-technology-desktop.jpg';
+        this.backgroundImage = `../assets/img/technology/background-technology-${this.windowClasification}.jpg`;
         break;
       default:
-        this.backgroundImage = '../assets/img/home/background-home-desktop.jpg';
+        this.backgroundImage = `../assets/img/home/background-home-${this.windowClasification}.jpg`;
         break;
     }
+  }
+
+  getWindowSizeClasification(): string {
+    if (this.innerWidth > 1000) { return 'desktop' } else
+    if (this.innerWidth < 1000 && this.innerWidth > 500) { return 'tablet' } else
+    if (this.innerWidth < 500) { return 'mobile' } else
+    return 'desktop';
   }
 }
