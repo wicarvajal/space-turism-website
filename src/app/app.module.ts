163f4a8a36bfea206cce1047376c3fa3ed2be06b
William Carvajal <wi.rcarvajal@gmail.com>
2021-11-30 20:42:15 -0300

    Fix h5 styles. DestinationDetail component added & TODO.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DestinationModule } from './modules/destination/destination.module';
 import { NavbarModule } from './modules/navbar/navbar.module';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { NavbarModule } from './modules/navbar/navbar.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavbarModule
+    NavbarModule,
+    DestinationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/destination/destination.module.ts
+++ src/app/modules/destination/destination.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DestinationPageComponent } from './destination-page/destination-page.component';
+import { DestinationDetailComponent } from './destination-detail/destination-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    DestinationPageComponent
+    DestinationPageComponent,
+    DestinationDetailComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  // exports: [
+  //   DestinationDetailComponent
+  // ]
 })
 export class DestinationModule { }
